13d008dcf37ee33312d18bd3b558496a56e9b412
Sb1414 <kalkamanova.sabina14@gmail.com>
2024-01-05 23:46:09 +0300

    add view

--- airport/airport/FlightsAddView.Designer.cs
+++ airport/airport/FlightsAddView.Designer.cs
@@ -28,29 +28,27 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.textBoxCode = new System.Windows.Forms.TextBox();
 			this.textBoxDepartureAirport = new System.Windows.Forms.TextBox();
 			this.buttonOk = new System.Windows.Forms.Button();
 			this.comboBoxDepartureAirport = new System.Windows.Forms.ComboBox();
 			this.comboBoxArrivalAirport = new System.Windows.Forms.ComboBox();
 			this.textBoxArrivalAirport = new System.Windows.Forms.TextBox();
 			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.departureDay = new System.Windows.Forms.DateTimePicker();
+			this.departureTime = new System.Windows.Forms.DateTimePicker();
+			this.arrivalTime = new System.Windows.Forms.DateTimePicker();
+			this.arrivalDay = new System.Windows.Forms.DateTimePicker();
 			this.SuspendLayout();
-			// 
-			// textBoxCode
-			// 
-			this.textBoxCode.BorderStyle = System.Windows.Forms.BorderStyle.None;
-			this.textBoxCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.textBoxCode.Location = new System.Drawing.Point(89, 204);
-			this.textBoxCode.Name = "textBoxCode";
-			this.textBoxCode.Size = new System.Drawing.Size(266, 20);
-			this.textBoxCode.TabIndex = 11;
 			// 
 			// textBoxDepartureAirport
 			// 
 			this.textBoxDepartureAirport.BorderStyle = System.Windows.Forms.BorderStyle.None;
 			this.textBoxDepartureAirport.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.textBoxDepartureAirport.Location = new System.Drawing.Point(89, 25);
+			this.textBoxDepartureAirport.Location = new System.Drawing.Point(49, 30);
 			this.textBoxDepartureAirport.Name = "textBoxDepartureAirport";
 			this.textBoxDepartureAirport.Size = new System.Drawing.Size(266, 20);
 			this.textBoxDepartureAirport.TabIndex = 9;
@@ -60,7 +58,7 @@
 			this.buttonOk.BackColor = System.Drawing.SystemColors.ActiveCaption;
 			this.buttonOk.FlatAppearance.BorderSize = 0;
 			this.buttonOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.buttonOk.Location = new System.Drawing.Point(143, 262);
+			this.buttonOk.Location = new System.Drawing.Point(102, 323);
 			this.buttonOk.Name = "buttonOk";
 			this.buttonOk.Size = new System.Drawing.Size(158, 33);
 			this.buttonOk.TabIndex = 8;
@@ -70,7 +68,7 @@
 			// comboBoxDepartureAirport
 			// 
 			this.comboBoxDepartureAirport.FormattingEnabled = true;
-			this.comboBoxDepartureAirport.Location = new System.Drawing.Point(89, 51);
+			this.comboBoxDepartureAirport.Location = new System.Drawing.Point(49, 56);
 			this.comboBoxDepartureAirport.Name = "comboBoxDepartureAirport";
 			this.comboBoxDepartureAirport.Size = new System.Drawing.Size(266, 24);
 			this.comboBoxDepartureAirport.TabIndex = 12;
@@ -78,7 +76,7 @@
 			// comboBoxArrivalAirport
 			// 
 			this.comboBoxArrivalAirport.FormattingEnabled = true;
-			this.comboBoxArrivalAirport.Location = new System.Drawing.Point(89, 149);
+			this.comboBoxArrivalAirport.Location = new System.Drawing.Point(49, 164);
 			this.comboBoxArrivalAirport.Name = "comboBoxArrivalAirport";
 			this.comboBoxArrivalAirport.Size = new System.Drawing.Size(266, 24);
 			this.comboBoxArrivalAirport.TabIndex = 14;
@@ -87,7 +85,7 @@
 			// 
 			this.textBoxArrivalAirport.BorderStyle = System.Windows.Forms.BorderStyle.None;
 			this.textBoxArrivalAirport.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.textBoxArrivalAirport.Location = new System.Drawing.Point(89, 123);
+			this.textBoxArrivalAirport.Location = new System.Drawing.Point(49, 138);
 			this.textBoxArrivalAirport.Name = "textBoxArrivalAirport";
 			this.textBoxArrivalAirport.Size = new System.Drawing.Size(266, 20);
 			this.textBoxArrivalAirport.TabIndex = 13;
@@ -95,23 +93,95 @@
 			// checkBox1
 			// 
 			this.checkBox1.AutoSize = true;
-			this.checkBox1.Location = new System.Drawing.Point(89, 81);
+			this.checkBox1.Location = new System.Drawing.Point(49, 86);
 			this.checkBox1.Name = "checkBox1";
 			this.checkBox1.Size = new System.Drawing.Size(223, 20);
 			this.checkBox1.TabIndex = 15;
 			this.checkBox1.Text = "изменить аэропорт отправки";
 			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(46, 11);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(269, 16);
+			this.label1.TabIndex = 18;
+			this.label1.Text = "Аэропорт отправки";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(46, 119);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(269, 16);
+			this.label2.TabIndex = 19;
+			this.label2.Text = "Аэропорт прибытия";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(46, 205);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(269, 16);
+			this.label3.TabIndex = 20;
+			this.label3.Text = "Дата вылета, время";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(46, 262);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(269, 16);
+			this.label4.TabIndex = 21;
+			this.label4.Text = "Дата прибытия, время";
+			// 
+			// departureDay
+			// 
+			this.departureDay.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+			this.departureDay.Location = new System.Drawing.Point(49, 227);
+			this.departureDay.Name = "departureDay";
+			this.departureDay.Size = new System.Drawing.Size(142, 22);
+			this.departureDay.TabIndex = 22;
+			// 
+			// departureTime
+			// 
+			this.departureTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+			this.departureTime.Location = new System.Drawing.Point(197, 227);
+			this.departureTime.Name = "departureTime";
+			this.departureTime.Size = new System.Drawing.Size(118, 22);
+			this.departureTime.TabIndex = 23;
+			// 
+			// arrivalTime
+			// 
+			this.arrivalTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+			this.arrivalTime.Location = new System.Drawing.Point(197, 281);
+			this.arrivalTime.Name = "arrivalTime";
+			this.arrivalTime.Size = new System.Drawing.Size(118, 22);
+			this.arrivalTime.TabIndex = 25;
+			// 
+			// arrivalDay
+			// 
+			this.arrivalDay.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+			this.arrivalDay.Location = new System.Drawing.Point(49, 281);
+			this.arrivalDay.Name = "arrivalDay";
+			this.arrivalDay.Size = new System.Drawing.Size(142, 22);
+			this.arrivalDay.TabIndex = 24;
 			// 
 			// FlightsAddView
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(445, 340);
+			this.ClientSize = new System.Drawing.Size(373, 378);
+			this.Controls.Add(this.arrivalTime);
+			this.Controls.Add(this.arrivalDay);
+			this.Controls.Add(this.departureTime);
+			this.Controls.Add(this.departureDay);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.checkBox1);
 			this.Controls.Add(this.comboBoxArrivalAirport);
 			this.Controls.Add(this.textBoxArrivalAirport);
 			this.Controls.Add(this.comboBoxDepartureAirport);
-			this.Controls.Add(this.textBoxCode);
 			this.Controls.Add(this.textBoxDepartureAirport);
 			this.Controls.Add(this.buttonOk);
 			this.Name = "FlightsAddView";
@@ -123,13 +193,19 @@
 		}
 
 		#endregion
-
-		private System.Windows.Forms.TextBox textBoxCode;
 		private System.Windows.Forms.TextBox textBoxDepartureAirport;
 		private System.Windows.Forms.Button buttonOk;
 		private System.Windows.Forms.ComboBox comboBoxDepartureAirport;
 		private System.Windows.Forms.ComboBox comboBoxArrivalAirport;
 		private System.Windows.Forms.TextBox textBoxArrivalAirport;
 		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.DateTimePicker departureDay;
+		private System.Windows.Forms.DateTimePicker departureTime;
+		private System.Windows.Forms.DateTimePicker arrivalTime;
+		private System.Windows.Forms.DateTimePicker arrivalDay;
 	}
 }
--- airport/airport/FlightsAddView.cs
+++ airport/airport/FlightsAddView.cs
@@ -2,25 +2,141 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace airport
 {
 	public partial class FlightsAddView : Form
 	{
-		public FlightsAddView()
+		string connectionString;
+		public FlightsAddView(string connectionString)
 		{
-			InitializeComponent();			
+			this.connectionString = connectionString;
+			InitializeComponent();
+			AddDepartureAirportMap();
+			AddArrivalAirportMap();
 
 			buttonOk.Text = "добавить";
 			buttonOk.DialogResult = DialogResult.OK;
 			this.AcceptButton = buttonOk;
 		}
 
+		public string DepartureTime => $"{departureDay.Value:yyyy-MM-dd} {departureTime.Value:HH:mm:ss}";
+		public string ArrivalTime => $"{arrivalDay.Value:yyyy-MM-dd} {arrivalTime.Value:HH:mm:ss}";
+		public int idDeparture => comboBoxDepartureAirport.SelectedItem != null ? (int)comboBoxDepartureAirport.SelectedValue : -1;
+		public int idArrival => comboBoxArrivalAirport.SelectedItem != null ? (int)comboBoxArrivalAirport.SelectedValue : -1;
 
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			if (checkBox1.Checked)
+			{
+				comboBoxDepartureAirport.Enabled = false;
+				textBoxDepartureAirport.Enabled = false;
+			} else
+			{
+				comboBoxDepartureAirport.Enabled = true;
+				textBoxDepartureAirport.Enabled = true;
+			}
+		}
+
+		private void AddDepartureAirportMap()
+		{
+			using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+			{
+				connection.Open();
+
+				string query = "SELECT id, City || ' - ' || Name || ' (' || Code || ')' AS airport FROM Airports";
+
+				Dictionary<int, string> airports = new Dictionary<int, string>();
+
+				using (SQLiteCommand cmd = new SQLiteCommand(query, connection))
+				{
+					using (SQLiteDataReader reader = cmd.ExecuteReader())
+					{
+						while (reader.Read())
+						{
+							int airports_id = Convert.ToInt32(reader["id"]);
+							string airports_name = reader["airport"].ToString();
+
+							airports.Add(airports_id, airports_name);
+						}
+					}
+				}
+				// сортировка данных в словаре
+				var sorted = airports.OrderBy(x => x.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+				// BindingSource для обновления данных ComboBox при изменении текста в TextBox
+				BindingSource bindingSource = new BindingSource(sorted, null);
+
+				comboBoxDepartureAirport.DataSource = bindingSource;
+				comboBoxDepartureAirport.DisplayMember = "Value";
+				comboBoxDepartureAirport.ValueMember = "Key";
+				comboBoxDepartureAirport.DropDownStyle = ComboBoxStyle.DropDownList;
+
+				// добавление обработчика события TextChanged для TextBox
+				textBoxDepartureAirport.TextChanged += (sender, e) =>
+				{
+					// фильтрация данных в ComboBox на основе введенного текста в TextBox
+					string searchText = textBoxDepartureAirport.Text.ToLower();
+					var filtered = sorted.Where(x => x.Value.ToLower().Contains(searchText)).ToDictionary(pair => pair.Key, pair => pair.Value);
+					bindingSource.DataSource = new BindingSource(filtered, null);
+				};
+
+				// установка выбранного элемента с id равным 1
+				comboBoxDepartureAirport.SelectedIndex = 1;
+				
+			}
+		}
+
+		private void AddArrivalAirportMap()
+		{
+			using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+			{
+				connection.Open();
+
+				string query = "SELECT id, City || ' - ' || Name || ' (' || Code || ')' AS airport FROM Airports";
+
+				Dictionary<int, string> airports = new Dictionary<int, string>();
+
+				using (SQLiteCommand cmd = new SQLiteCommand(query, connection))
+				{
+					using (SQLiteDataReader reader = cmd.ExecuteReader())
+					{
+						while (reader.Read())
+						{
+							int airports_id = Convert.ToInt32(reader["id"]);
+							string airports_name = reader["airport"].ToString();
+
+							airports.Add(airports_id, airports_name);
+						}
+					}
+				}
+				// сортировка данных в словаре
+				var sorted = airports.OrderBy(x => x.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+				// BindingSource для обновления данных ComboBox при изменении текста в TextBox
+				BindingSource bindingSource = new BindingSource(sorted, null);
+
+				comboBoxArrivalAirport.DataSource = bindingSource;
+				comboBoxArrivalAirport.DisplayMember = "Value";
+				comboBoxArrivalAirport.ValueMember = "Key";
+				comboBoxArrivalAirport.DropDownStyle = ComboBoxStyle.DropDownList;
+
+				// добавление обработчика события TextChanged для TextBox
+				textBoxArrivalAirport.TextChanged += (sender, e) =>
+				{
+					// фильтрация данных в ComboBox на основе введенного текста в TextBox
+					string searchText = textBoxArrivalAirport.Text.ToLower();
+					var filtered = sorted.Where(x => x.Value.ToLower().Contains(searchText)).ToDictionary(pair => pair.Key, pair => pair.Value);
+					bindingSource.DataSource = new BindingSource(filtered, null);
+				};
+			}
+		}
 	}
 }
